a1d9650a3a53568b43f04aaf5aefe91e2f1f81b6
Fuedyolo <frederik.ommundsen@gmail.com>
2023-01-20 10:23:49 +0100

    added dockerfile for application
    Doesn't work yet

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import UploadFile, File
-from typing import Optional
-import cv2
 from fastapi.responses import FileResponse
 
 
 import pandas as pd
-#df=pd.read_excel('Datafile.xlsx')
-#df.iloc[0].values[0]
+
+# df=pd.read_excel('Datafile.xlsx')
+# df.iloc[0].values[0]
 from io import BytesIO
 
 import torch
@@ -15,26 +14,25 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 from src_2.models.model import GCN
-import matplotlib.pyplot as plt 
-import hydra
+import matplotlib.pyplot as plt
 from hydra.utils import get_original_cwd
-import os
-import wandb
 from torch_geometric.nn import GCNConv
 from fastapi import FastAPI
+
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
-   return {"Hello": "World"}
-
+    return {"Hello": "World"}
 
 
 from http import HTTPStatus
 
+
 @app.get("/")
 def root():
-    """ Health check."""
+    """Health check."""
     response = {
         "message": HTTPStatus.OK.phrase,
         "status-code": HTTPStatus.OK,
@@ -58,8 +56,10 @@ class GCN(torch.nn.Module):
 
         return F.log_softmax(x, dim=1)
 
-#state_dict = torch.load('outputs/2023-01-13/22-17-30/trained_model.pt')
-#print(10)
+
+# state_dict = torch.load('outputs/2023-01-13/22-17-30/trained_model.pt')
+# print(10)
+
 
 @app.post("/predict_model_v4/")
 async def cv_model(efile: UploadFile = File(...)):
@@ -67,20 +67,19 @@ async def cv_model(efile: UploadFile = File(...)):
     data = dataset[0]
 
     model = GCN(dataset.num_node_features, dataset.num_classes)
-    state_dict = torch.load('outputs/2023-01-13/22-17-30/trained_model.pt')
+    state_dict = torch.load("outputs/2023-01-13/22-17-30/trained_model.pt")
     model.load_state_dict(state_dict)
     model.eval()
     with torch.no_grad():
         pred = model(data).argmax(dim=1)
         correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
         acc = int(correct) / int(data.test_mask.sum())
-        #print(f'Accuracy: {acc:.4f}')
-    
-    response = {
-      "input": 10,
-      "message": HTTPStatus.OK.phrase,
-      "status-code": HTTPStatus.OK,
-      "output": acc
-   }
-    return response
+        # print(f'Accuracy: {acc:.4f}')
 
+    response = {
+        "input": 10,
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+        "output": acc,
+    }
+    return response
