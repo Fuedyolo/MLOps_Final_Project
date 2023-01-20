b72be4d9b023e83ddc4bf82ce63dea7a4c4f58ad
fontaluc <fontaluc@gmail.com>
2023-01-20 18:05:46 +0100

    Update test

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,12 +3,10 @@ import os.path
 import pytest
 import torch
 
-from src_2.models.train_model import main
-from tests import _PATH_DATA
+data_path = 'data/processed/dataset.pt'
 
-
-@pytest.mark.skipif(not os.path.exists(_PATH_DATA + '/dataset.pt'), reason="Data files not found")
+@pytest.mark.skipif(not os.path.exists(data_path), reason="Data files not found")
 def test_data():
-    dataset = torch.load(_PATH_DATA + '/dataset.pt')
+    dataset = torch.load(data_path)
     assert dataset.num_classes==6, "Dataset did not have the correct number of classes"
     assert dataset[0]['x'].shape == torch.Size([3327, 3703]), "Dataset did not have the correct shape"
